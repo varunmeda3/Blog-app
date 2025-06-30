063f242a6876ea2b82e79e9a5bd52eaf0932b37f
varunmeda3 <medavarun3@gmail.com>
2025-06-30 17:41:43 -0400

    Update _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,1 @@
-import '@/styles/globals.css'
-import 'bootstrap/dist/css/bootstrap.css';
-import { useEffect } from 'react';
 
-
-export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
-  }, [])
-  return <Component {...pageProps} />
-}
